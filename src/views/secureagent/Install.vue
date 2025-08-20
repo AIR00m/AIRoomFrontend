49660d44c01c14049b76b9ea3c8072694612ab49
Jihyuck <wlgur4772@gmail.com>
2025-08-20 16:56:05 +0900

    feat: agent download url

--- src/views/secureagent/Install.vue
+++ src/views/secureagent/Install.vue
@@ -233,7 +233,6 @@ export default {
 
     function downloadUrl() {
       return import.meta.env.DEV ? 'http://localhost:8080/download/agent' : 'http://43.200.2.244:8080/download/agent'
-      
     }
 
     function goDownload() {
