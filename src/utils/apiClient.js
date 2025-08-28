7f13d7b44916f9f878b9a288c315dbcbaf68b44a
nunus18 <wosik123@naver.com>
2025-08-28 18:06:11 +0900

    package.json 업데이트

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -5,8 +5,8 @@
  * - Bearer 토큰 자동 포함
  */
 
-const API_BASE_URL = "http://localhost:8080";
-//const API_BASE_URL = "http://43.200.2.244:8080";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "http://43.200.2.244:8080";
 
 class ApiClient {
   constructor(baseURL = API_BASE_URL) {
